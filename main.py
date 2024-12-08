673119c2aba899157a7567699ea18820f44fe381
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-12-08 15:17:59 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter 1st number "))
+num2 = int(input("Enter 2nd number "))
+num3 = int(input("Enter 3rd number "))
+num4 = int(input("Enter 4th number "))
+num5 = int(input("Enter 5th number "))
+numbers = (num1,num2, num3, num4, num5)
+print(numbers)
+tuplex = num1 * num2 * num3 * num4 * num5
+print(tuplex)
